3f2680397320400eeeca711f284490e7a4314173
Marco Di Lisa <marco.di.lisa@nttdata.com>
2023-04-05 10:38:29 +0200

    [IDP-627] Fixed class TransactionInProgress

--- src/main/java/it/gov/pagopa/payment/model/TransactionInProgress.java
+++ src/main/java/it/gov/pagopa/payment/model/TransactionInProgress.java
@@ -1,8 +1,18 @@
 package it.gov.pagopa.payment.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldNameConstants;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("transaction_in_progress")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldNameConstants
+@Document(collection = "transaction_in_progress")
 public class TransactionInProgress {
 
 }
