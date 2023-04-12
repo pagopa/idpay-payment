8d72a4cb5c596253a6fe69e127754920505c37cc
sbellezz <sbellezz@emeal.nttdata.com>
2023-04-12 14:34:41 +0200

    [IDP-640] moved userId to header

--- src/main/java/it/gov/pagopa/payment/controller/TransactionController.java
+++ src/main/java/it/gov/pagopa/payment/controller/TransactionController.java
@@ -2,14 +2,11 @@ package it.gov.pagopa.payment.controller;
 
 import it.gov.pagopa.payment.dto.TransactionDTO;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/idpay/payment/transaction")
 public interface TransactionController {
-    @GetMapping("/{transactionId}/{userId}")
+    @GetMapping("/{transactionId}")
     @ResponseStatus(code = HttpStatus.OK)
-    TransactionDTO getTransaction(@PathVariable String transactionId, @PathVariable String userId);
+    TransactionDTO getTransaction(@PathVariable String transactionId, @RequestHeader("x-user-id") String userId);
 }
