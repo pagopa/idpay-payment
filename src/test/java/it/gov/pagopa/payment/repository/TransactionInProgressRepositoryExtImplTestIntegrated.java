79d83b19bc1cf4c1cbc6a91acb14ec6d5f99c15a
Marco Di Lisa <marco.di.lisa@nttdata.com>
2023-05-04 16:08:14 +0200

    [IDP-779] Migration to Gradle - Ignoring integrated tests in gradle build

--- src/test/java/it/gov/pagopa/payment/repository/TransactionInProgressRepositoryExtImplTestIntegrated.java
+++ src/test/java/it/gov/pagopa/payment/repository/TransactionInProgressRepositoryExtImplTestIntegrated.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.payment.repository;
 
+import org.junit.jupiter.api.Tag;
 import org.springframework.test.context.TestPropertySource;
 
 @SuppressWarnings({"squid:S3577", "NewClassNamingConvention"}) // suppressing class name not match alert: we are not using the Test suffix in order to let not execute this test by default maven configuration because it depends on properties not pushable
@@ -7,5 +8,6 @@ import org.springframework.test.context.TestPropertySource;
         "classpath:/mongodbEmbeddedDisabled.properties",
         "classpath:/secrets/mongodbConnectionString.properties"
 })
+@Tag("integrationTest") //using tag to exclude this class from being executed by Gradle test task because it depends on properties not pushable
 class TransactionInProgressRepositoryExtImplTestIntegrated extends TransactionInProgressRepositoryExtImplTest {
 }
