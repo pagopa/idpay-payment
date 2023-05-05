c7ff2d53dd0364c1e3987e9224d6069df2aebc72
Giuseppe-LaManna <Giuseppe.LaManna@emeal.nttdata.com>
2023-05-05 18:12:27 +0200

    IDP-839  Update TransactionNotifierServiceImpl, added configuration for producer

--- src/main/java/it/gov/pagopa/payment/service/TransactionNotifierServiceImpl.java
+++ src/main/java/it/gov/pagopa/payment/service/TransactionNotifierServiceImpl.java
@@ -1,12 +1,20 @@
 package it.gov.pagopa.payment.service;
 
 import it.gov.pagopa.payment.model.TransactionInProgress;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TransactionNotifierServiceImpl implements TransactionNotifierService {
+    @Value("${spring.cloud.stream.bindings.transactionNotifier-out-0.binder}")
+    private String binder;
+    private final StreamBridge streamBridge;
+    public TransactionNotifierServiceImpl(StreamBridge streamBridge) {
+        this.streamBridge = streamBridge;
+    }
     @Override
     public boolean notify(TransactionInProgress trx) {
-        return true; //TODO notify into idpay-transactions kafka queue
+       return streamBridge.send("transactionNotifier-out-0",binder,trx);
     }
 }
