8f3b3bff00070c6489d8318aafd40a50ae38c22c
anttorre <antonio.torre@nttdata.com>
2023-05-12 15:39:20 +0200

    rejection reasons mapper handling

--- src/main/java/it/gov/pagopa/payment/dto/mapper/TransactionInProgress2TransactionOutcomeDTOMapper.java
+++ src/main/java/it/gov/pagopa/payment/dto/mapper/TransactionInProgress2TransactionOutcomeDTOMapper.java
@@ -2,17 +2,36 @@ package it.gov.pagopa.payment.dto.mapper;
 
 import it.gov.pagopa.payment.model.TransactionInProgress;
 import it.gov.pagopa.payment.model.TransactionOutcomeDTO;
+import it.gov.pagopa.payment.utils.RewardConstants;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import org.springframework.stereotype.Service;
 
 @Service
 public class TransactionInProgress2TransactionOutcomeDTOMapper implements Function<TransactionInProgress, TransactionOutcomeDTO> {
 
   @Override
   public TransactionOutcomeDTO apply(TransactionInProgress trx) {
-    return TransactionOutcomeDTO.builder()
+      List<String> rejectionReasons = Collections.emptyList();
+      Map<String, List<String>> initiativeRejectionReasons;
+
+      if(!CollectionUtils.isEmpty(trx.getRejectionReasons())){
+          if(trx.getRejectionReasons().contains(RewardConstants.TRX_REJECTION_REASON_NO_INITIATIVE)){
+              rejectionReasons = List.of(RewardConstants.TRX_REJECTION_REASON_NO_INITIATIVE);
+          }
+          initiativeRejectionReasons = Map.of(
+                  trx.getInitiativeId(),
+                  trx.getRejectionReasons()
+          );
+      } else {
+          initiativeRejectionReasons = Collections.emptyMap();
+      }
+
+      return TransactionOutcomeDTO.builder()
         .id(trx.getId())
         .trxCode(trx.getTrxCode())
         .idTrxAcquirer(trx.getIdTrxAcquirer())
@@ -33,12 +52,10 @@ public class TransactionInProgress2TransactionOutcomeDTOMapper implements Functi
         .merchantFiscalCode(trx.getMerchantFiscalCode())
         .vat(trx.getVat())
         .initiativeId(trx.getInitiativeId())
+        .reward(trx.getReward())
         .rewards(trx.getRewards())
-        .rejectionReasons(trx.getRejectionReasons())
-        .initiativeRejectionReasons(Map.of(
-            trx.getInitiativeId(),
-            Collections.emptyList()
-        ))
+        .rejectionReasons(rejectionReasons)
+        .initiativeRejectionReasons(initiativeRejectionReasons)
         .userId(trx.getUserId())
         .status(trx.getStatus())
         .channel(trx.getChannel())
