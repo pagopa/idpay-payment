e13b11e80d8ddf230ebd13a2a7f34a5a2f5650ee
GiovanaSolorzano <gisolorz@emeal.nttdata.com>
2023-05-31 14:31:33 +0200

    IDP-1102 update MerchantDetailDTO

--- src/main/java/it/gov/pagopa/payment/connector/rest/merchant/dto/MerchantDetailDTO.java
+++ src/main/java/it/gov/pagopa/payment/connector/rest/merchant/dto/MerchantDetailDTO.java
@@ -5,13 +5,25 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class MerchantDetailDTO {
+    private String initiativeId;
     private String initiativeName;
-    private String businessName;
+    private String businessName; //ragione sociale
+    private String legalOfficeAddress;
+    private String legalOfficeMunicipality; // comune sede legale
+    private String legalOfficeProvince;
+    private String legalOfficeZipCode;
+    private String certifiedEmail;
     private String fiscalCode;
     private String vatNumber;
+    private String status;
+    private String iban;
+    private LocalDateTime creationDate;
+    private LocalDateTime updateDate;
 }
