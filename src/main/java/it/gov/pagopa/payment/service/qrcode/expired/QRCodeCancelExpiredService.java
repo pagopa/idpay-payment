8d882c321ebd189ae6b4b0862a69d0ca1c8aa6e1
GiovanaSolorzano <gisolorz@emeal.nttdata.com>
2023-06-13 17:10:16 +0200

    IDP-1140 Added expiration service

--- src/main/java/it/gov/pagopa/payment/service/qrcode/expired/QRCodeCancelExpiredService.java
+++ src/main/java/it/gov/pagopa/payment/service/qrcode/expired/QRCodeCancelExpiredService.java
@@ -4,4 +4,5 @@ package it.gov.pagopa.payment.service.qrcode.expired;
  * This component schedules the expiration of cancelled payments
  * */
 public interface QRCodeCancelExpiredService {
+    void execute();
 }
