fa98e9e39f423f4c14d64388306e92d95e6f9d3f
Ranaldo Daniele <Daniele.Ranaldo@emea.nttdata.com>
2023-06-20 15:29:39 +0200

    [IDP-1229] added rewardAmount and fixed effectiveAmount

--- src/main/java/it/gov/pagopa/payment/dto/MerchantTransactionDTO.java
+++ src/main/java/it/gov/pagopa/payment/dto/MerchantTransactionDTO.java
@@ -1,13 +1,13 @@
 package it.gov.pagopa.payment.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
@@ -19,7 +19,9 @@ public class MerchantTransactionDTO {
     String trxCode;
     String trxId;
     String fiscalCode;
-    BigDecimal effectiveAmount;
+    @NotNull
+    Long effectiveAmount;
+    Long rewardAmount;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     LocalDateTime trxDate;
     Integer trxExpirationMinutes;
