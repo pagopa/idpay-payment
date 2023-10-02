db53182dafb6aed3264a7d2c82381852f52ba880
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2023-10-02 18:00:37 +0200

    update controller

--- src/main/java/it/gov/pagopa/payment/controller/CommonPaymentController.java
+++ src/main/java/it/gov/pagopa/payment/controller/CommonPaymentController.java
@@ -1,4 +1,18 @@
 package it.gov.pagopa.payment.controller;
 
+import it.gov.pagopa.payment.dto.qrcode.TransactionCreationRequest;
+import it.gov.pagopa.payment.dto.qrcode.TransactionResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/idpay/payment/")
 public interface CommonPaymentController {
+    @PostMapping("/merchant")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    TransactionResponse createTransaction(
+            @RequestBody @Valid TransactionCreationRequest trxCreationRequest,
+            @RequestHeader("x-merchant-id") String merchantId,
+            @RequestHeader("x-acquirer-id") String acquirerId,
+            @RequestHeader("x-apim-request-id") String idTrxIssuer);
 }
--- src/main/java/it/gov/pagopa/payment/controller/CommonPaymentControllerImpl.java
+++ src/main/java/it/gov/pagopa/payment/controller/CommonPaymentControllerImpl.java
@@ -1,7 +1,25 @@
 package it.gov.pagopa.payment.controller;
 
+import it.gov.pagopa.common.performancelogger.PerformanceLog;
+import it.gov.pagopa.payment.dto.qrcode.TransactionCreationRequest;
+import it.gov.pagopa.payment.dto.qrcode.TransactionResponse;
+import it.gov.pagopa.payment.service.performancelogger.TransactionResponsePerfLoggerPayloadBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 public class CommonPaymentControllerImpl implements CommonPaymentController {
+
+    @Override
+    @PerformanceLog(
+            value = "CREATE_TRANSACTION",
+            payloadBuilderBeanClass = TransactionResponsePerfLoggerPayloadBuilder.class)
+    public TransactionResponse createTransaction(
+            TransactionCreationRequest trxCreationRequest,
+            String merchantId,
+            String acquirerId,
+            String idTrxIssuer) {
+        log.info("[CREATE_TRANSACTION] The merchant {} through acquirer {} is creating a transaction", merchantId, acquirerId);
+        return null; //TODO common service
+    }
 }
