6a6b80fb9f58980ff2d1295113d76be963ca7b63
SalvatorManna <salvatore11.manna@emeal.nttdata.com>
2023-10-11 12:26:49 +0200

    IDP-1855-Modified bar code integration tests

--- src/test/java/it/gov/pagopa/payment/controller/payment/BarCodePaymentControllerIntegrationTest.java
+++ src/test/java/it/gov/pagopa/payment/controller/payment/BarCodePaymentControllerIntegrationTest.java
@@ -2,20 +2,33 @@ package it.gov.pagopa.payment.controller.payment;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import it.gov.pagopa.payment.BaseIntegrationTest;
+import it.gov.pagopa.payment.constants.PaymentConstants;
 import it.gov.pagopa.payment.dto.barcode.AuthBarCodePaymentDTO;
 import it.gov.pagopa.payment.dto.barcode.TransactionBarCodeCreationRequest;
 import it.gov.pagopa.payment.dto.barcode.TransactionBarCodeResponse;
+import it.gov.pagopa.payment.dto.qrcode.SyncTrxStatusDTO;
 import it.gov.pagopa.payment.dto.qrcode.TransactionCreationRequest;
 import it.gov.pagopa.payment.dto.qrcode.TransactionResponse;
+import it.gov.pagopa.payment.enums.InitiativeRewardType;
+import it.gov.pagopa.payment.enums.SyncTrxStatus;
+import it.gov.pagopa.payment.model.InitiativeConfig;
+import it.gov.pagopa.payment.model.RewardRule;
+import it.gov.pagopa.payment.model.TransactionInProgress;
+import it.gov.pagopa.payment.repository.RewardRuleRepository;
+import it.gov.pagopa.payment.repository.TransactionInProgressRepository;
 import it.gov.pagopa.payment.test.fakers.TransactionCreationRequestFaker;
 import it.gov.pagopa.payment.utils.RewardConstants;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.io.UnsupportedEncodingException;
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -23,11 +36,19 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 class BarCodePaymentControllerIntegrationTest extends BaseIntegrationTest {
 
+    @Autowired
+    private RewardRuleRepository rewardRuleRepository;
+    @Autowired
+    private TransactionInProgressRepository transactionInProgressRepository;
+
     protected String getChannel() {
         return RewardConstants.TRX_CHANNEL_BARCODE;
     }
     private final String USERID = "USERID";
     private final String MERCHANTID = "MERCHANTID";
+    public static final String INITIATIVEID = "INITIATIVEID";
+    public static final String INITIATIVEID_NOT_STARTED = INITIATIVEID + "1";
+    public static final LocalDate TODAY = LocalDate.now();
 
 
     protected MvcResult createTrx(TransactionBarCodeCreationRequest trxRequest, String userId) throws Exception {
@@ -51,6 +72,7 @@ class BarCodePaymentControllerIntegrationTest extends BaseIntegrationTest {
                 .andReturn();
     }
 
+    /*
     @Test
     void createTransaction_InitiativeNotExist() throws Exception {
         // useCase 0: initiative not existent
@@ -61,6 +83,48 @@ class BarCodePaymentControllerIntegrationTest extends BaseIntegrationTest {
         // Other APIs cannot be invoked because we have not a valid trxId
         TransactionBarCodeResponse dummyTrx = TransactionBarCodeResponse.builder().id("DUMMYTRXID").trxCode("dummytrxcode").trxDate(OffsetDateTime.now()).build();
         extractResponse(authTrx(dummyTrx, USERID, MERCHANTID), HttpStatus.NOT_FOUND, null);
+    }
+     */
+
+    @Test
+    void authorizeTransaction_budgetExhausted() throws Exception{
+        TransactionBarCodeCreationRequest trxRequest = TransactionBarCodeCreationRequest.builder()
+                .initiativeId(INITIATIVEID)
+                .build();
+        AuthBarCodePaymentDTO authBarCodePaymentDTO = AuthBarCodePaymentDTO.builder().amountCents(10000L).build();
+        rewardRuleRepository.save(RewardRule.builder().id(INITIATIVEID)
+                .initiativeConfig(InitiativeConfig.builder()
+                        .initiativeId(INITIATIVEID)
+                        .initiativeRewardType(InitiativeRewardType.DISCOUNT)
+                        .startDate(TODAY.minusDays(1))
+                        .endDate(TODAY.plusDays(1))
+                        .build())
+                .build());
+
+        // Creating transaction
+        TransactionResponse trxCreated = extractResponse(createTrx(trxRequest, USERID), HttpStatus.CREATED, TransactionResponse.class);
+
+        // Cannot invoke other APIs if REJECTED
+        //extractResponse(authTrx(trxCreated.getTrxCode(), authBarCodePaymentDTO, MERCHANTID), HttpStatus.BAD_REQUEST, null);
+    }
+
+    @Test
+    void useCase20_createTransaction_invalidInitiativePeriod() throws Exception{
+        rewardRuleRepository.save(RewardRule.builder().id(INITIATIVEID_NOT_STARTED)
+                .initiativeConfig(InitiativeConfig.builder()
+                        .initiativeId(INITIATIVEID_NOT_STARTED)
+                        .initiativeRewardType(InitiativeRewardType.DISCOUNT)
+                        .startDate(TODAY.plusDays(1))
+                        .endDate(TODAY.plusDays(1))
+                        .build())
+                .build());
+
+        TransactionBarCodeCreationRequest trxRequest = TransactionBarCodeCreationRequest.builder()
+                .initiativeId(INITIATIVEID_NOT_STARTED)
+                .build();
+
+        // Creating transaction
+        extractResponse(createTrx(trxRequest, USERID), HttpStatus.BAD_REQUEST, null);
     }
 
     protected <T> T extractResponse(MvcResult response, HttpStatus expectedHttpStatusCode, Class<T> expectedBodyClass) {
