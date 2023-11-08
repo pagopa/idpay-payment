a9757fd1a4ced9ac9acb9e8fcdcd23c432bcb4d0
frtrinca <francesco.trinca@nttdata.com>
2023-11-08 16:29:10 +0100

    Fix IDP 2113 - fix response dto validaitons

--- src/main/java/it/gov/pagopa/payment/dto/AuthPaymentDTO.java
+++ src/main/java/it/gov/pagopa/payment/dto/AuthPaymentDTO.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import it.gov.pagopa.payment.enums.SyncTrxStatus;
 import it.gov.pagopa.payment.model.counters.RewardCounters;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,25 +20,16 @@ import java.util.Map;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class AuthPaymentDTO {
 
-  @NotBlank
   private String id;
-  @NotBlank
   private String trxCode;
-  @NotBlank
   private OffsetDateTime trxDate;
-  @NotBlank
   private String initiativeId;
-  @NotBlank
   private String initiativeName;
-  @NotBlank
   private String businessName;
-  @NotNull
   private SyncTrxStatus status;
   private Long reward;
   private RewardCounters counters;
-  @NotNull
   private List<String> rejectionReasons;
-  @NotNull
   private Long amountCents;
 
   private Long residualBudget;
--- src/main/java/it/gov/pagopa/payment/dto/qrcode/TransactionResponse.java
+++ src/main/java/it/gov/pagopa/payment/dto/qrcode/TransactionResponse.java
@@ -1,8 +1,6 @@
 package it.gov.pagopa.payment.dto.qrcode;
 
 import it.gov.pagopa.payment.enums.SyncTrxStatus;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,29 +13,17 @@ import java.time.OffsetDateTime;
 @NoArgsConstructor
 @Builder
 public class TransactionResponse {
-  @NotBlank
   private String id;
-  @NotBlank
   private String trxCode;
-  @NotBlank
   private String initiativeId;
-  @NotBlank
   private String merchantId;
-  @NotBlank
   private String idTrxIssuer;
-  @NotBlank
   private String idTrxAcquirer;
-  @NotNull
   private OffsetDateTime trxDate;
-  @NotNull
   private Long amountCents;
-  @NotBlank
   private String amountCurrency;
-  @NotBlank
   private String mcc;
-  @NotBlank
   private String acquirerId;
-  @NotNull
   private SyncTrxStatus status;
   private String qrcodePngUrl;
   private String qrcodeTxtUrl;
