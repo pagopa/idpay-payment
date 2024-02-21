ba47292b1aefe7e13bc1646acacfc370dc6bc6b9
DanieleRanaldo <Daniele.Ranaldo@nttdata.com>
2024-02-21 12:01:09 +0100

    [IDP-2306] initializing timeout consumer

--- src/main/java/it/gov/pagopa/payment/event/consumer/TimeoutConsumer.java
+++ src/main/java/it/gov/pagopa/payment/event/consumer/TimeoutConsumer.java
@@ -3,12 +3,11 @@ package it.gov.pagopa.payment.event.consumer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
 
 import java.util.function.Consumer;
 @Slf4j
 @Configuration
 public class TimeoutConsumer {
     @Bean
-    public Consumer<Message<String>> consume() { 	return a -> log.info(a + "[TIMEOUT-CONSUMER-TEST]"); 	}
+    public Consumer<String> consume() { 	return a -> log.info(a + "[TIMEOUT-CONSUMER-TEST]"); 	}
 }
