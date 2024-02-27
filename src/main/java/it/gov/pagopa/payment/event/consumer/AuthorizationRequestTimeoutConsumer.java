41346aacfc1da789719e79f7cd2838933a7c4ea2
DanieleRanaldo <124155243+DanieleRanaldo@users.noreply.github.com>
2024-02-27 10:32:31 +0100

    feat: IDP 2306 new consumer for timeout payment (#253)
    [IDP-2306] new authorization timeout consumer

---
+++ src/main/java/it/gov/pagopa/payment/event/consumer/AuthorizationRequestTimeoutConsumer.java
@@ -0,0 +1,24 @@
+package it.gov.pagopa.payment.event.consumer;
+
+import it.gov.pagopa.payment.service.AuthorizationRequestTimeoutService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+
+import java.util.function.Consumer;
+@Slf4j
+@Configuration
+public class AuthorizationRequestTimeoutConsumer {
+    private final AuthorizationRequestTimeoutService authorizationRequestTimeoutService;
+
+    public AuthorizationRequestTimeoutConsumer(AuthorizationRequestTimeoutService authorizationRequestTimeoutService) {
+        this.authorizationRequestTimeoutService = authorizationRequestTimeoutService;
+    }
+
+
+    @Bean
+    public Consumer<Message<String>> paymentTimeoutConsumer() {
+        return authorizationRequestTimeoutService::execute;
+    }
+}
