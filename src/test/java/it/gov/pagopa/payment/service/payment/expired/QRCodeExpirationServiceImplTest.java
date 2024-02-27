1216a2cbe5ecc92dc2fdaa9b8b2c9b5f05a18015
stefanodel <stefano11.delia@nttdata.com>
2024-02-27 17:15:40 +0100

    [IDP-2311] update junit

--- src/test/java/it/gov/pagopa/payment/service/payment/expired/QRCodeExpirationServiceImplTest.java
+++ src/test/java/it/gov/pagopa/payment/service/payment/expired/QRCodeExpirationServiceImplTest.java
@@ -1,8 +1,6 @@
 package it.gov.pagopa.payment.service.payment.expired;
 
 import it.gov.pagopa.payment.connector.rest.reward.RewardCalculatorRestClient;
-import it.gov.pagopa.payment.enums.SyncTrxStatus;
-import it.gov.pagopa.payment.model.TransactionInProgress;
 import it.gov.pagopa.payment.repository.TransactionInProgressRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,22 +9,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.OffsetDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @ExtendWith(MockitoExtension.class)
 class QRCodeExpirationServiceImplTest{
-
-    private static final int N = 20;
-    private static final int N_EXPIRED = N/2;
-
-    private OffsetDateTime OFFSET_NOW;
-    private OffsetDateTime AUTHORIZATION_EXPIRED_DATE;
-    private OffsetDateTime CANCEL_EXPIRED_DATE;
-
     @Mock
     private RewardCalculatorRestClient rewardCalculatorRestClientSpy;
 
@@ -40,12 +24,6 @@ class QRCodeExpirationServiceImplTest{
     private TransactionInProgressRepository repository;
 
     private QRCodeExpirationServiceImpl qrCodeExpirationServiceImpl;
-
-    private final List<TransactionInProgress> trxs = new ArrayList<>(N);
-    private final Map<SyncTrxStatus, List<TransactionInProgress>> expiredTrxs = new HashMap<>();
-    private final Map<SyncTrxStatus, List<TransactionInProgress>> validTrxs = new HashMap<>();
-
-
 
     @BeforeEach
     void init() {
@@ -75,6 +53,15 @@ class QRCodeExpirationServiceImplTest{
         Mockito.when(cancelExpiredService.execute()).thenReturn(1L);
         qrCodeExpirationServiceImpl.scheduleCancelExpired();
         Mockito.verify(cancelExpiredService,Mockito.times(1)).execute();
+
+    }
+
+    @Test
+    void forceConfirmTrxExpiration() {
+        // waitFor expired trxs deleted from db
+        Mockito.when(cancelExpiredService.forceExpiration("INITIATIVEID")).thenReturn(1L);
+        qrCodeExpirationServiceImpl.forceConfirmTrxExpiration("INITIATIVEID");
+        Mockito.verify(cancelExpiredService,Mockito.times(1)).forceExpiration("INITIATIVEID");
 
     }
 
