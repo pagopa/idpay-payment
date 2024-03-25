6b7a30be12b1fca06cbeaa6a6f29a0f3ac57937d
GiovanaSolorzano <giovanaelvira.solorzanochirieleison@nttdata.com>
2024-03-25 18:07:23 +0100

    [IDP-2366] fix annotation for coverage

--- src/main/java/it/gov/pagopa/common/kafka/service/ErrorNotifierInfoDTO.java
+++ src/main/java/it/gov/pagopa/common/kafka/service/ErrorNotifierInfoDTO.java
@@ -1,15 +1,11 @@
 package it.gov.pagopa.common.kafka.service;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.messaging.Message;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class ErrorNotifierInfoDTO {
     private String srcType;
     private String srcServer;
