3e2ee7f850ba05a7b99a1d1908c2d98e1ed43c0e
stefanodel <stefano11.delia@nttdata.com>
2024-09-17 14:36:55 +0200

    chore: update spring boot version

--- src/main/java/it/gov/pagopa/common/config/CustomMongoHealthIndicator.java
+++ src/main/java/it/gov/pagopa/common/config/CustomMongoHealthIndicator.java
@@ -1,26 +1,36 @@
 package it.gov.pagopa.common.config;
+
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.springframework.boot.actuate.data.mongo.MongoHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CustomMongoHealthIndicator implements HealthIndicator {
+public class CustomMongoHealthIndicator extends MongoHealthIndicator {
 
     private final MongoTemplate mongoTemplate;
 
     public CustomMongoHealthIndicator(MongoTemplate mongoTemplate) {
+        super(mongoTemplate);
         this.mongoTemplate = mongoTemplate;
     }
 
     @Override
-    public Health health() {
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
         try {
-            mongoTemplate.executeCommand("{ ping: 1 }");
+            // Esegui un comando MongoDB personalizzato, ad esempio un ping
+            MongoDatabase database = mongoTemplate.getDb();
+            Document result = database.runCommand(new Document("ping", 1));
 
-            return Health.up().withDetail("message", "Custom MongoDB health check passed").build();
+            if (result.getDouble("ok") == 1.0) {
+                builder.up().withDetail("Ping result", "OK");
+            } else {
+                builder.down().withDetail("Ping result", "Failed");
+            }
         } catch (Exception e) {
-            return Health.down().withDetail("error", e.getMessage()).build();
+            builder.down(e);
         }
     }
 }
