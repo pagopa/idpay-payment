e9b05f4a84dd88b703efb95e68486d82a4fee94e
stefanodel <stefano11.delia@nttdata.com>
2024-09-26 17:47:54 +0200

    fix: test call

--- src/main/java/it/gov/pagopa/common/config/CustomMongoHealthIndicator.java
+++ src/main/java/it/gov/pagopa/common/config/CustomMongoHealthIndicator.java
@@ -1,59 +1,37 @@
 package it.gov.pagopa.common.config;
 
-import com.mongodb.client.MongoDatabase;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
-import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.data.mongo.MongoHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.stereotype.Component;
-import org.springframework.util.Assert;
-
-@Component
 @Slf4j
-public class CustomMongoHealthIndicator extends AbstractHealthIndicator {
+public class CustomMongoHealthIndicator extends MongoHealthIndicator {
 
     private final MongoTemplate mongoTemplate;
-    private Health cachedHealth = null;
-    private long lastCheckTime = 0;
-    private static final long CACHE_TTL_MS = 10000; // Cache valida per 10 secondi
 
-    public CustomMongoHealthIndicator(MongoTemplate mongoTemplate) {
-        super("MongoDB health check failed");
-        Assert.notNull(mongoTemplate, "MongoTemplate must not be null");
+    public CustomMongoHealthIndicator(MongoTemplate mongoTemplate){
+        super(mongoTemplate);
         this.mongoTemplate = mongoTemplate;
     }
 
     @Override
-    protected void doHealthCheck(Health.Builder builder) throws Exception {
-        long currentTime = System.currentTimeMillis();
-
-   /*     // Restituisci risultato cache se non scaduto
-        if (cachedHealth != null && (currentTime - lastCheckTime) < CACHE_TTL_MS) {
-            builder.status(cachedHealth.getStatus()).withDetails(cachedHealth.getDetails());
-            log.debug("[HEALTH MONGODB - CACHED] Restituito risultato dalla cache.");
-            return;
-        } */
-
-        // Esegui controllo Mongo e aggiorna cache
-        lastCheckTime = currentTime;
+    public void doHealthCheck(Health.Builder builder) throws Exception {
         try {
-            MongoDatabase database = mongoTemplate.getDb();
-            Document result = database.runCommand(new Document("ping", 1));
+            // Esecuzione del comando ping
+            Document pingResult = mongoTemplate.executeCommand(new Document("ping", 1));
 
-            if (result.getDouble("ok") == 1.0) {
-                builder.up().withDetail("Ping result", "OK");
-                log.info("[HEALTH MONGODB - UP] Ping result: OK");
+            // Controlla se il ping ha avuto successo
+            if (pingResult != null && pingResult.getInteger("ok", 0) == 1) {
+                builder.up().withDetail("pingResult", pingResult);
+                log.info("Ping OK: {}", pingResult);
             } else {
-                builder.down().withDetail("Ping result", "Failed");
-                log.error("[HEALTH MONGODB - DOWN] Ping result: Failed");
+                log.error("Ping failed: {}", pingResult);
+                builder.down();
             }
-
         } catch (Exception e) {
-            builder.down(e);
+            log.error("Error executing ping command: {}", e.getMessage());
+            builder.down();
         }
-
-        // Aggiorna la cache con il nuovo risultato
-        cachedHealth = builder.build();
     }
 }
