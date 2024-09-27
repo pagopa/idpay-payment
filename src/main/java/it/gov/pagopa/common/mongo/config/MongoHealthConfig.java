9b10780bad77b424aa662408054bb3035d167420
stefanodel <stefano11.delia@nttdata.com>
2024-09-27 16:22:35 +0200

    fix: test call

--- src/main/java/it/gov/pagopa/common/mongo/config/MongoHealthConfig.java
+++ src/main/java/it/gov/pagopa/common/mongo/config/MongoHealthConfig.java
@@ -4,12 +4,14 @@ import it.gov.pagopa.common.config.CustomMongoHealthIndicator;
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class MongoHealthConfig {
     @Bean
-        public AbstractHealthIndicator mongoHealthIndicator(MongoTemplate mongoTemplate) {
+    @Primary
+    public AbstractHealthIndicator mongoHealthIndicator(MongoTemplate mongoTemplate) {
         return new CustomMongoHealthIndicator(mongoTemplate);
     }
 }
