4543113f2db52d409d53ea60dcc979fb50515b37
stefanodel <stefano11.delia@nttdata.com>
2024-09-27 18:08:17 +0200

    fix: test call

--- src/main/java/it/gov/pagopa/common/mongo/config/MongoHealthConfig.java
+++ src/main/java/it/gov/pagopa/common/mongo/config/MongoHealthConfig.java
@@ -1,18 +1,31 @@
 package it.gov.pagopa.common.mongo.config;
 
 import it.gov.pagopa.common.config.CustomMongoHealthIndicator;
+import it.gov.pagopa.common.config.HealthIndicatorLogger;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+import java.util.List;
+
 @Configuration
-public class MongoHealthConfig {
+public class MongoHealthConfig    {
     @Bean
-    @Primary
-    public HealthIndicator customMongoHealthIndicator(MongoTemplate mongoTemplate) {
+    public CustomMongoHealthIndicator customMongoHealthIndicator(MongoTemplate mongoTemplate) {
         return new CustomMongoHealthIndicator(mongoTemplate);
+    }
+
+    @Bean
+    public HealthIndicatorLogger healthIndicatorLogger(List<HealthIndicator> healthIndicatorList) {
+        // Assicurati che non ci sia duplicazione della classe CustomMongoHealthIndicator
+        healthIndicatorList.removeIf(this::isCustomMongoHealthIndicator);
+        return new HealthIndicatorLogger(healthIndicatorList);
+    }
+
+    // Method to check if an indicator is an instance of CustomMongoHealthIndicator
+    private boolean isCustomMongoHealthIndicator(HealthIndicator indicator) {
+        return indicator instanceof CustomMongoHealthIndicator;
     }
 }
 
