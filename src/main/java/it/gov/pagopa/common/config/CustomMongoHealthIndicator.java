6cb1c837fc6dbfd2a211c5accc672a5e1b37b856
stefanodel <stefano11.delia@nttdata.com>
2024-09-30 15:50:25 +0200

    fix: add isMaster MongoDB Health check

--- src/main/java/it/gov/pagopa/common/config/CustomMongoHealthIndicator.java
+++ src/main/java/it/gov/pagopa/common/config/CustomMongoHealthIndicator.java
@@ -1,11 +1,12 @@
 package it.gov.pagopa.common.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
 import org.springframework.boot.actuate.data.mongo.MongoHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-
+@Slf4j
 public class CustomMongoHealthIndicator extends MongoHealthIndicator {
 
     private final MongoTemplate mongoTemplate;
@@ -19,6 +20,7 @@ public class CustomMongoHealthIndicator extends MongoHealthIndicator {
     protected void doHealthCheck(Health.Builder builder) throws Exception {
        Document result = this.mongoTemplate.executeCommand("{ isMaster: 1 }");
        builder.up().withDetail("maxWireVersion", result.getInteger("maxWireVersion"));
+       log.info("[MONGODB]: UP");
     }
 
 
