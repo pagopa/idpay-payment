2446e803a3c7f9cd03cac9733d7d0542845631f8
stefanodel <stefano11.delia@nttdata.com>
2024-10-01 12:08:01 +0200

    Revert "fix: test delete HealthIndicatorLogger"
    This reverts commit 1e31d8c91ae0c505593987f76cfee9ad059349ba.

--- src/main/java/it/gov/pagopa/common/mongo/config/MongoHealthConfig.java
+++ src/main/java/it/gov/pagopa/common/mongo/config/MongoHealthConfig.java
@@ -1,15 +1,29 @@
 package it.gov.pagopa.common.mongo.config;
 
 import it.gov.pagopa.common.config.CustomMongoHealthIndicator;
+import it.gov.pagopa.common.config.HealthIndicatorLogger;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+import java.util.List;
+
 @Configuration
-public class MongoHealthConfig {
+public class MongoHealthConfig    {
     @Bean
     public CustomMongoHealthIndicator customMongoHealthIndicator(MongoTemplate mongoTemplate) {
         return new CustomMongoHealthIndicator(mongoTemplate);
+    }
+
+    @Bean
+    public HealthIndicatorLogger healthIndicatorLogger(List<HealthIndicator> healthIndicatorList) {
+        healthIndicatorList.removeIf(this::isCustomMongoHealthIndicator);
+        return new HealthIndicatorLogger(healthIndicatorList);
+    }
+
+    private boolean isCustomMongoHealthIndicator(HealthIndicator indicator) {
+        return indicator instanceof CustomMongoHealthIndicator;
     }
 }
 
