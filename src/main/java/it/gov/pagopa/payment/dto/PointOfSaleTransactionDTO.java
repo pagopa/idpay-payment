e6c51c5b7008737e885606eb748c174c689ec48d
BaldiVi-1 <vittorio.baldi@emeal.nttdata.com>
2025-07-30 19:00:40 +0200

    feat: [EBE-69] New API to retrieve in-progress transactions (#319) (#320)
    * [EBE-69] Implemented API to retrieve in progress transactions for a specific point of sale
    
    * [EBE-69] Removed code duplication using helper method
    
    * [EBE-69] Fix code format
    
    * [EBE-69] Avoid decrypting userId when fiscalCode input is already available
    
    * [EBE-69] Removed redundant condition before forEach on transactions
    
    * [EBE-69] Refactor encryptCF and decryptCF to use a common wrapper for error handling
    
    * [EBE-69] Isolate controller/service layers by moving DTOs to controller; encapsulate criteria and count logic in repository with single paginated call from service
    
    * [EBE-69] Refactor PDVServiceImpl: simplify wrapPDVCall by removing generics
    
    Co-authored-by: fedcurciorubertini <federico.curciorubertini@nttdata.com>

---
+++ src/main/java/it/gov/pagopa/payment/dto/PointOfSaleTransactionDTO.java
@@ -0,0 +1,38 @@
+package it.gov.pagopa.payment.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import it.gov.pagopa.payment.enums.SyncTrxStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PointOfSaleTransactionDTO {
+
+    private String trxCode;
+    private String trxId;
+    private String fiscalCode;
+    @NotNull
+    private Long effectiveAmountCents;
+    private Long rewardAmountCents;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime trxDate;
+    private Long trxExpirationSeconds;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime updateDate;
+    private SyncTrxStatus status;
+    private Boolean splitPayment;
+    private Long residualAmountCents;
+    private String channel;
+    private String qrcodePngUrl;
+    private String qrcodeTxtUrl;
+}
