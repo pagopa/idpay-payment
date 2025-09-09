97c44a567e45b84647394c3010b04f9d1d5e85d4
michael11barone <Michael.Barone@nttdata.com>
2025-09-09 16:41:27 +0200

    Added additionalProperties for gtin in BarcodeAuthPaymentServiceImpl + Prevented potential log injection

--- src/main/java/it/gov/pagopa/payment/service/payment/barcode/BarCodeAuthPaymentServiceImpl.java
+++ src/main/java/it/gov/pagopa/payment/service/payment/barcode/BarCodeAuthPaymentServiceImpl.java
@@ -119,5 +119,6 @@ public class BarCodeAuthPaymentServiceImpl implements BarCodeAuthPaymentService 
         trx.setVat(merchantDetail.getVatNumber());
         trx.setAcquirerId(acquirerId);
         trx.setAmountCurrency(PaymentConstants.CURRENCY_EUR);
+        trx.setAdditionalProperties(authBarCodePaymentDTO.getAdditionalProperties());
     }
 }
