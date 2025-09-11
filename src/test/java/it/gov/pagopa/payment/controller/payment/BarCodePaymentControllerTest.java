f834ccf380600202576535ab138423a3214e7d17
BaldiVi-1 <vittorio.baldi@emeal.nttdata.com>
2025-09-11 11:30:26 +0200

    fix: resolve CVE (#321)
    Co-authored-by: Andrea Morabito <78792023+and-mora@users.noreply.github.com>
    Co-authored-by: angelosca24 <angelo.scagliola@nttdata.com>

--- src/test/java/it/gov/pagopa/payment/controller/payment/BarCodePaymentControllerTest.java
+++ src/test/java/it/gov/pagopa/payment/controller/payment/BarCodePaymentControllerTest.java
@@ -20,7 +20,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -40,7 +40,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Import({JsonConfig.class, PaymentErrorManagerConfig.class, ValidationExceptionHandler.class})
 class BarCodePaymentControllerTest {
 
-    @MockBean
+    @MockitoBean
     private BarCodePaymentService barCodePaymentService;
 
     @Autowired
