e5d6ff4c4f79bc1d3242f15558e93b1d87035c76
EMORONGD6 <guido.moroni@emeal.nttdata.com>
2025-09-12 17:24:10 +0200

    feat: working on log injection

--- src/main/java/it/gov/pagopa/payment/utils/Utilities.java
+++ src/main/java/it/gov/pagopa/payment/utils/Utilities.java
@@ -7,7 +7,10 @@ public final class Utilities {
     private Utilities() {}
 
     public static String sanitizeString(String str){
+        if(str == null) {
+            return null;
+        }
         return str.replaceAll("[\\r\\n]", "") // remove new line and carriage return
-                .replaceAll("[^\\w\\s-\\[\\]]", ""); // allow only alphanumeric, whitespace, dash, square brackets
+                .replaceAll("[^\\w\\s-]", ""); // allow only alphanumeric, whitespace, dash
     }
 }
