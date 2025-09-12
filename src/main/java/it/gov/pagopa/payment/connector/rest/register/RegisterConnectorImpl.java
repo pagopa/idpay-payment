4d2be1994a40a908237afe2a8a4d31fd23cf84b2
BaldiVi-1 <vittorio.baldi@emeal.nttdata.com>
2025-09-12 12:18:13 +0200

    add log in register connector in case success or catch

--- src/main/java/it/gov/pagopa/payment/connector/rest/register/RegisterConnectorImpl.java
+++ src/main/java/it/gov/pagopa/payment/connector/rest/register/RegisterConnectorImpl.java
@@ -4,9 +4,11 @@ import feign.FeignException;
 import it.gov.pagopa.payment.connector.rest.register.dto.ProductListDTO;
 import it.gov.pagopa.payment.connector.rest.register.dto.ProductStatus;
 import it.gov.pagopa.payment.exception.custom.ProductInvocationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class RegisterConnectorImpl implements RegisterConnector {
 
     private final RegisterRestClient restClient;
@@ -20,7 +22,9 @@ public class RegisterConnectorImpl implements RegisterConnector {
         ProductListDTO productListDTO;
         try {
             productListDTO = restClient.getProductList(productGtin, status);
+            log.info("[REGISTER_CONNECTOR] Successfully retrieved products from register ProductGtin: {} and ProductStatus: {}, ReturnedCount: {}",productGtin,status, productListDTO != null ? productListDTO.getContent().size() : 0);
         } catch (FeignException e) {
+            log.error("[REGISTER_CONNECTOR] Error calling register service ProductGtin: {} and ProductStatus: {}, message: {}",productGtin, status, e.getMessage());
             throw new ProductInvocationException(
                     "An error occurred in the microservice payment", true, e);
         }
