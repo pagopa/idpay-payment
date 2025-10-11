5e486d7713e779b5ec3c0abfe6ec38a01a1c85e8
dario.pelliccioli <dario.pelliccioli@nttdata.com>
2025-10-11 10:50:38 +0200

    fix configuration AppConfigurationProperties.ExtendedTransactions

--- src/main/java/it/gov/pagopa/payment/configuration/AppConfigurationProperties.java
+++ src/main/java/it/gov/pagopa/payment/configuration/AppConfigurationProperties.java
@@ -1,17 +1,18 @@
 package it.gov.pagopa.payment.configuration;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "app")
 public class AppConfigurationProperties {
 
-    @Getter
-    @Setter
-    public class ExtendedTransactions {
+    @Configuration
+    @ConfigurationProperties(prefix = "app.extendedTransactions")
+    @Data
+    public static class ExtendedTransactions {
         int staleMinutesThreshold;
         int sendExpiredSendBatchSize;
         int updateBatchSize;
