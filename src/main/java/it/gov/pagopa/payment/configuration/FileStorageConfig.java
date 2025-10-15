4d7efc2c2c4add7d83eeff4bb94470539ac8b015
Christian Geovese <cgenoves@emeal.nttdata.com>
2025-10-15 16:55:42 +0200

    apply @ConfigurationProperties best practice for FileStorageConfig

--- src/main/java/it/gov/pagopa/payment/configuration/FileStorageConfig.java
+++ src/main/java/it/gov/pagopa/payment/configuration/FileStorageConfig.java
@@ -2,23 +2,30 @@ package it.gov.pagopa.payment.configuration;
 
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FileStorageConfig {
 
-  @Bean
-  public BlobContainerClient fileStorageClientConfiguration(
-    @Value("${blobStorage.storageAccountName}") String storageAccountName,
-    @Value("${blobStorage.containerReference}") String containerName) {
+  private final FileStorageProperties properties;
 
+  public FileStorageConfig(FileStorageProperties properties) {
+    this.properties = properties;
+  }
+
+  @Bean
+  public BlobServiceClient blobServiceClient() {
     return new BlobServiceClientBuilder()
+        .endpoint("https://" + properties.getStorageAccountName() + ".blob.core.windows.net")
         .credential(new DefaultAzureCredentialBuilder().build())
-        .endpoint("https://" + storageAccountName + ".blob.core.windows.net")
-        .buildClient()
-        .getBlobContainerClient(containerName);
+        .buildClient();
+  }
+
+  @Bean
+  public BlobContainerClient blobContainerClient(BlobServiceClient blobServiceClient) {
+    return blobServiceClient.getBlobContainerClient(properties.getContainerReference());
   }
 }
