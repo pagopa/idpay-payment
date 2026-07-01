fbbdcc338d92f230fdd28fd3dc5c9f885374e102
BaldiVi-1 <vittorio.baldi@emeal.nttdata.com>
2026-07-01 11:44:07 +0200

    delete local test

--- src/main/java/it/gov/pagopa/payment/repository/TransactionOutboxRepository.java
+++ src/main/java/it/gov/pagopa/payment/repository/TransactionOutboxRepository.java
@@ -1,21 +1,7 @@
 package it.gov.pagopa.payment.repository;
 
-import it.gov.pagopa.payment.entity.TransactionOutbox;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface TransactionOutboxRepository extends JpaRepository<TransactionOutbox, String>{
 
-    List<TransactionOutbox> findTop100ByPublishedFalseOrderByCreatedAtAsc();
-
-    @Modifying
-    @Query("""
-            update TransactionOutbox t
-            set t.published = true
-            where t.id = :id
-            """)
-    void markAsPublished(Long id);
 }
