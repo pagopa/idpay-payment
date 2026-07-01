35aeb3e1d78802c985b7b3e55f74936464f4c91e
BaldiVi-1 <vittorio.baldi@emeal.nttdata.com>
2026-07-01 12:00:17 +0200

    delete unused repository

--- src/main/java/it/gov/pagopa/payment/repository/TransactionOutboxRepository.java
+++
@@ -1,7 +0,0 @@
-package it.gov.pagopa.payment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionOutboxRepository extends JpaRepository<TransactionOutbox, String>{
-
-}
