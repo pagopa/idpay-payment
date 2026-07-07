915bc8815e17344c60248596d5ec5529ef323515
BaldiVi-1 <vittorio.baldi@emeal.nttdata.com>
2026-07-07 12:25:05 +0200

    resolve issues

--- src/test/java/it/gov/pagopa/payment/service/payment/expired/idpaycode/IdpayCodeAuthorizationExpiredServiceImplTest.java
+++ src/test/java/it/gov/pagopa/payment/service/payment/expired/idpaycode/IdpayCodeAuthorizationExpiredServiceImplTest.java
@@ -16,7 +16,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
